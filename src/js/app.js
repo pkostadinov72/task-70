e9a6299cbc97edaeaa2fbe4141574c4678ba1c18
Plamenpowa <pkostadinov72@gmail.com>
2022-11-30 14:27:35 +0200

    scale image on click

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  const div = document.querySelector(".image");
+  div.addEventListener("click", () => {
+    div.style.scale = "2";
+  });
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
